b813b368151a68bcb8cc627c79937e04140e48a4
Nguyễn Tuấn Anh <anhnt.fami@gmail.com>
2014-03-21 23:28:48 +0700

    fix & preview

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,5 +1,126 @@
 'use strict';
 
+// ok, we're good
+var readCanvasData = function(oCanvas) {
+    var iWidth = parseInt(oCanvas.width);
+    var iHeight = parseInt(oCanvas.height);
+    return oCanvas.getContext("2d").getImageData(0,0,iWidth,iHeight);
+}
+
+// base64 encodes either a string or an array of charcodes
+var encodeData = function(data) {
+    var strData = "";
+    if (typeof data == "string") {
+        strData = data;
+    } else {
+        var aData = data;
+        for (var i=0;i<aData.length;i++) {
+            strData += String.fromCharCode(aData[i]);
+        }
+    }
+    return btoa(strData);
+}
+
+// creates a base64 encoded string containing BMP data
+// takes an imagedata object as argument
+var createBMP = function(oData) {
+    var aHeader = [];
+
+    var iWidth = oData.width;
+    var iHeight = oData.height;
+
+    aHeader.push(0x42); // magic 1
+    aHeader.push(0x4D); 
+
+    var iFileSize = iWidth*iHeight*3 + 54; // total header size = 54 bytes
+    aHeader.push(iFileSize % 256); iFileSize = Math.floor(iFileSize / 256);
+    aHeader.push(iFileSize % 256); iFileSize = Math.floor(iFileSize / 256);
+    aHeader.push(iFileSize % 256); iFileSize = Math.floor(iFileSize / 256);
+    aHeader.push(iFileSize % 256);
+
+    aHeader.push(0); // reserved
+    aHeader.push(0);
+    aHeader.push(0); // reserved
+    aHeader.push(0);
+
+    aHeader.push(54); // dataoffset
+    aHeader.push(0);
+    aHeader.push(0);
+    aHeader.push(0);
+
+    var aInfoHeader = [];
+    aInfoHeader.push(40); // info header size
+    aInfoHeader.push(0);
+    aInfoHeader.push(0);
+    aInfoHeader.push(0);
+
+    var iImageWidth = iWidth;
+    aInfoHeader.push(iImageWidth % 256); iImageWidth = Math.floor(iImageWidth / 256);
+    aInfoHeader.push(iImageWidth % 256); iImageWidth = Math.floor(iImageWidth / 256);
+    aInfoHeader.push(iImageWidth % 256); iImageWidth = Math.floor(iImageWidth / 256);
+    aInfoHeader.push(iImageWidth % 256);
+
+    var iImageHeight = iHeight;
+    aInfoHeader.push(iImageHeight % 256); iImageHeight = Math.floor(iImageHeight / 256);
+    aInfoHeader.push(iImageHeight % 256); iImageHeight = Math.floor(iImageHeight / 256);
+    aInfoHeader.push(iImageHeight % 256); iImageHeight = Math.floor(iImageHeight / 256);
+    aInfoHeader.push(iImageHeight % 256);
+
+    aInfoHeader.push(1); // num of planes
+    aInfoHeader.push(0);
+
+    aInfoHeader.push(24); // num of bits per pixel
+    aInfoHeader.push(0);
+
+    aInfoHeader.push(0); // compression = none
+    aInfoHeader.push(0);
+    aInfoHeader.push(0);
+    aInfoHeader.push(0);
+
+    var iDataSize = iWidth*iHeight*3; 
+    aInfoHeader.push(iDataSize % 256); iDataSize = Math.floor(iDataSize / 256);
+    aInfoHeader.push(iDataSize % 256); iDataSize = Math.floor(iDataSize / 256);
+    aInfoHeader.push(iDataSize % 256); iDataSize = Math.floor(iDataSize / 256);
+    aInfoHeader.push(iDataSize % 256); 
+
+    for (var i=0;i<16;i++) {
+        aInfoHeader.push(0);    // these bytes not used
+    }
+
+    var iPadding = (4 - ((iWidth * 3) % 4)) % 4;
+
+    var aImgData = oData.data;
+
+    var strPixelData = "";
+    var y = iHeight;
+    do {
+        var iOffsetY = iWidth*(y-1)*4;
+        var strPixelRow = "";
+        for (var x=0;x<iWidth;x++) {
+            var iOffsetX = 4*x;
+
+            strPixelRow += String.fromCharCode(aImgData[iOffsetY+iOffsetX+2]);
+            strPixelRow += String.fromCharCode(aImgData[iOffsetY+iOffsetX+1]);
+            strPixelRow += String.fromCharCode(aImgData[iOffsetY+iOffsetX]);
+        }
+        for (var c=0;c<iPadding;c++) {
+            strPixelRow += String.fromCharCode(0);
+        }
+        strPixelData += strPixelRow;
+    } while (--y);
+
+    var strEncoded = encodeData(aHeader.concat(aInfoHeader)) + encodeData(strPixelData);
+
+    return strEncoded;
+}
+
+function buildBMP(oCanvas) {
+
+    var oData = readCanvasData(oCanvas);
+    var strImgData = createBMP(oData);
+    return strImgData;
+}
+
 function maxhide(width, height, blockLength, infoBlockLength){
     var numberOfPixels = width * height,
         numberOfBlocks = Math.floor(numberOfPixels/blockLength),
@@ -181,7 +302,7 @@ angular.module('se10th20132App')
                             canvas.height = img.height;
                             var context = canvas.getContext('2d');
                             context.drawImage(img, 0, 0, img.width, img.height);
-                            task.bmpLink = URL.createObjectURL(b64toBlob(Canvas2Image.createBMP(canvas), 'image/bmp'));
+                            task.bmpLink = URL.createObjectURL(b64toBlob(buildBMP(canvas), 'image/bmp'));
                             $scope.$apply();
                         }
                         img.src = task.image_link;
